7ccea5af6566b4233d8fe71fd63690321982a07d
Eric Kofoid <eric@kofoid.net>
2024-03-02 07:34:13 -0600

    add requrements

--- Home.py
+++ Home.py
@@ -6,6 +6,7 @@ st.title("Streamlit & Back 2 Viz Basics")
 
 st.markdown("Things I want to learn doing this project:")
 st.markdown("* Streamlit")
+st.markdown("* Python viz libraries")
 st.markdown("* Pandas")
 st.markdown("* Data Viz in Streamlit")
 st.markdown("* Github Copilot")
